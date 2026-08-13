cc8304ad8c9385c87b25adc1bcf01fa38ec4a342
Peter Svensson <psvensson@gmail.com>
2026-08-13 20:50:07 +0200

    Add value kind tags

---
+++ src/value/kinds.js
@@ -0,0 +1,11 @@
+const VALUE_KIND = Object.freeze({
+  BOOLEAN: 'boolean',
+  INTEGER: 'integer',
+  FLOAT64: 'float64',
+  TEXT: 'text',
+  BYTES: 'bytes',
+  REF: 'ref',
+  PINNED_REF: 'pinned-ref',
+});
+
+export {VALUE_KIND};
