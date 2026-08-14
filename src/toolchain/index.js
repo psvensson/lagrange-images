d33245c36e1f7714b69e6fcdaadf5a9b245033fc
Peter Svensson <psvensson@gmail.com>
2026-08-14 10:56:46 +0200

    Share generic raw WASM artifact constant

--- src/toolchain/index.js
+++ src/toolchain/index.js
@@ -1,8 +1,26 @@
-export * from './cargo-rustc-oci-provider.js';
+export {
+  CARGO_RUSTC_OCI_PROVIDER_ID,
+  CARGO_RUSTC_OCI_PROVIDER_V0,
+  CARGO_RUSTC_OCI_PROVIDER_V1,
+  CARGO_VENDOR_CONFIG_V1,
+  CargoRustcOciBuildError,
+  RUST_CARGO_CONFIG_V1,
+  RUST_CARGO_LOCK_V1,
+  RUST_CARGO_MANIFEST_V1,
+  RUST_CARGO_VENDOR_FILE_V1,
+  RUST_SOURCE_V1,
+  cargoBuildCommand,
+  cargoOutputPath,
+  materializeCargoProject,
+  normalizePortableProjectPath,
+  normalizeVendorPath,
+  validateVendorPackages,
+} from './cargo-rustc-oci-provider.js';
 export {
   CARGO_RUSTC_OCI_CACHE_CONTRACT_V0,
   createCacheableCargoRustcOciProvider as createCargoRustcOciProvider,
 } from './cacheable-cargo-rustc-oci-provider.js';
+export {WASM_BINARY_V1} from '../wasm/foreign-artifacts.js';
 export * from './derivation-cache.js';
 export * from './oci-cli-runner.js';
 export * from './provider-registry.js';
