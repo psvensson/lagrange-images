8611e97b896c7c2c112872284435cd297b2782c0
Peter Svensson <psvensson@gmail.com>
2026-08-14 07:16:10 +0200

    register wasm executor

--- src/execution/executor.js
+++ src/execution/executor.js
@@ -1,3 +1,5 @@
+import {WASM_FUNCTION_V1} from '../code/wasm-artifacts.js';
+import {wasmFunctionV1Executor} from '../wasm/executor.js';
 import {ActivationExecutor} from './activation-executor.js';
 import {CodeExecutorRegistry} from './executor-registry.js';
 import {
@@ -8,6 +10,7 @@ import {
 function createDefaultCodeExecutorRegistry() {
   const registry = new CodeExecutorRegistry();
   registry.register(NEUTRAL_EXPRESSION_V0, neutralExpressionV0Executor);
+  registry.register(WASM_FUNCTION_V1, wasmFunctionV1Executor);
   return registry;
 }
 
